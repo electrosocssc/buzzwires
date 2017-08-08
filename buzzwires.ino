8608ae78d260184fa81c52c755a4c1e6204a2be4
Abhishek Chakraborty <deadbeatfour@gmail.com>
2017-08-08 05:46:59 +0530

    Added conductor calibration
    Now it should work for any conductor. On boot, the capacitive sensor is
    calibrated against readings for the connected conductor (if any)

--- buzzwires.ino
+++ buzzwires.ino
@@ -1,11 +1,19 @@
 #include <CapacitiveSensor.h>
 
+long read_cap, max_cal = 0;
 CapacitiveSensor buzzwire = CapacitiveSensor(7,2);        // 220K resistor between pins 7 & 2, pin 2 is sensor pin, add a wire and or foil if desired
 void setup()                    
 {
    buzzwire.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate
    Serial.begin(9600);
    pinMode(13, OUTPUT); // Set the on-board LED as indicator
+   // Calibration
+
+   for (int i = 0 ; i < 100 ; i++ ){
+      read_cap = buzzwire.capacitiveSensor(60);
+      max_cal = max_cal < read_cap? read_cap : max_cal;
+   }
+   
 
 }
 
@@ -14,10 +22,11 @@ void loop()
     
     long total =  buzzwire.capacitiveSensor(60); // Read the sensed capacitance summed over 60 samples
 
-    if ( total > 50 ) { // 50 is the noise threshold
+    if ( total > max_cal + 50 ) { // 50 is the noise threshold
       digitalWrite(13, HIGH);  // Turn on onboard LED for 1s and turn it off again. 
       delay(1000);
       digitalWrite(13, LOW);
     }
+    Serial.println(total);
     delay(100);                             // arbitrary delay
 }
